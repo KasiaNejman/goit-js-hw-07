d88e5cd3ba8991b6474fa83c70daf1d8a24adbeb
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2023-12-13 10:35:54 +0100

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,5 @@
 import { galleryItems } from "./gallery-items.js";
 
-// Tworzenie znacznika dla każdego elementu galerii
 const createGalleryItemMarkup = ({ preview, original, description }) => `
   <li>
     <a class="gallery__item" href="${original}">
@@ -9,13 +8,11 @@ const createGalleryItemMarkup = ({ preview, original, description }) => `
   </li>
 `;
 
-// Wyszukiwanie kontenera dla galerii
 const galleryContainer = document.querySelector(".gallery");
-// Tworzenie znaczników dla całej galerii i dołączanie ich do kontenera
-const galleryMarkup = galleryItems.map(createGalleryItemMarkup).join("");
-galleryContainer.innerHTML = galleryMarkup; // Poprawione, usunięcie dodatkowego <ul>
 
-// Inicjalizacja SimpleLightbox po załadowaniu DOM i dodaniu elementów galerii
+const galleryMarkup = galleryItems.map(createGalleryItemMarkup).join("");
+galleryContainer.innerHTML = galleryMarkup;
+
 document.addEventListener("DOMContentLoaded", () => {
   new SimpleLightbox(".gallery a", {
     captionsData: "alt",
